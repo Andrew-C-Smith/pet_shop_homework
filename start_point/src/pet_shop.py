5eb44a5fa66ea30c9edfd4da487e327c07198a84
Andrew Smith <andrew_c_smith@hotmail.com>
2022-04-01 13:50:16 +0100

    homework Q3

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -6,4 +6,12 @@ def get_pet_shop_name(pet_shop):
 
 def get_total_cash(cash):
     return cash["admin"]["total_cash"]
+
+
+def add_or_remove_cash(total_new_cash ,cash ):
+    total_new_cash["admin"]["total_cash"] +=cash
+    return total_new_cash
     
+
+
+   
